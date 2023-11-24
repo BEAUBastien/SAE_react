05e194e7458a7ca0e2afb929ecd5971c60330e70
BEAUBastien <202223babas@gmail.com>
2023-11-24 10:46:34 +0100

    form_nb_joueur

--- src/ConfigPage.js
+++ src/ConfigPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function ConfigPage() {
-  return (
-    <div>
-      <h2>ConfigPage</h2>
-      <p>TEST CONFIG PAGE WORKING</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>ConfigPage</h2>
+            <form action="" method="get" class="form_config">
+            <select name="nb_joueurs" id="nb_joueurs">
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            <Link to='/game'>
+                <input type="submit" value="Valider" />
+            </Link>
+            </form>
+
+        </div>
+    );
 }
 
 export default ConfigPage;
