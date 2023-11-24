fa8e9fb41de6df272860033c7e64a5f629758044
Keran Saint-Blancat <keran.blancat@mediatropy.com>
2023-11-24 17:33:40 +0100

    k2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import LaunchPage from './LaunchPage';
@@ -6,18 +6,29 @@ import ConfigPage from './ConfigPage';
 import GameHub from './GameHub';
 
 function App() {
-    return (
-        <Router>
-      <div>
-        <Routes>
-        {/* Définissez vos routes avec le composant Route */}
-        <Route path="/" element={<LaunchPage/>} />
-        <Route path="/config" element={<ConfigPage />} />
-        <Route path="/hub/:pin" element={<GameHub />} />
-        {/* <Route path="/game/:pin" element={<Game />} /> */}
-        </Routes>
-      </div>
-    </Router>
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+    //     <Router>
+    //   <div>
+    //     <Routes>
+    //     {/* Définissez vos routes avec le composant Route */}
+    //     <Route path="/" element={<LaunchPage/>} />
+    //     <Route path="/config" element={<ConfigPage />} />
+    //     <Route path="/hub/:pin" element={<GameHub />} />
+    //     {/* <Route path="/game/:pin" element={<Game />} /> */}
+    //     </Routes>
+    //   </div>
+    // </Router>
         
     );
 }
