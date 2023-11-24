ebd2850f1e4154b6f567c571693a341ff10fb97c
BEAUBastien <202223babas@gmail.com>
2023-11-24 09:51:59 +0100

    Init React

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+    return (
+        <div>
+            <h1>Mon Premier Composant React</h1>
+            <p>Bienvenue dans le monde de React!</p>
+        </div>
+    );
+}
+export default App;
