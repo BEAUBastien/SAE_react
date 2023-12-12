38c70f0cb91befd756370e611fbbce2726625f37
Keran Saint-Blancat <keran.blancat@mediatropy.com>
2023-12-12 06:39:54 +0100

    premier test de game master sans le fonc

--- src/Game.js
+++ src/Game.js
@@ -8,18 +8,39 @@ import { DataSnapshot, getDatabase, onValue, ref, set, update } from 'firebase/d
 function Game() {
     return (
         <div class="main-container">
-            <h1 id='Titre'>ROARRR !</h1>
-            <div>
-                <h3 id='bienvenue'>Bienvenue</h3>
-                {/* <h3>{data}</h3> */}
-            </div>
+            {deroulement === 'start' && <Start />}
+            {deroulement === 'cupidon' && <Cupidon />}
+            {deroulement === 'voyante' && <Voyante />}
+            {deroulement === 'loup' && <Loups />}
+            {deroulement === 'sorciere' && <Sorciere />}
+            {deroulement === 'jour' && <Jour />}
+            {deroulement === 'winLoup' && <WinLoup />}
+            {joueur.role === 'loup' && <WinVillageois />}
+            {deroulement === 'chasseur' && <Chasseur />}
+            {deroulement === 'maire' && <Maire />}
+            {deroulement === 'voteVillage' && <VoteVillage />}
+            {deroulement === 'nuit' && <Nuit />}
+
+
+            {/* {deroulement === 'start' && <Start />}
+            {deroulement === 'cupidon' && <Cupidon />}
+            {deroulement === 'voyante' && joueur.role === 'voyante' && joueur.etat === 'vivant' && <Voyante />}
+            {deroulement === 'loup' && <Loups />}
+            {deroulement === 'sorciere' && joueur.role === 'sorciere' && joueur.etat === 'vivant' ? <Sorciere /> : (deroulement = 'jour')}
+            {deroulement === 'jour' && joueur.role !== 'loup' && joueur.etat === 'vivant' ? <Jour /> : (deroulement = 'winLoup')}
+            {(deroulement === 'winLoup' || joueur.role !== 'loup' && joueur.etat === 'vivant' === 0) && <WinLoup />}
+            {joueur.role === 'loup' && joueur.etat === 'vivant' === 0 && <WinVillageois />}
+            {deroulement === 'chasseur' && <Chasseur />}
+            {deroulement === 'maire' && <Maire />}
+            {deroulement === 'voteVillage' && <VoteVillage />}
+            {deroulement === 'nuit' && <Nuit />} */}
         </div>
     );
 }
 
-function changeDeroulement(partieId, deroulement){
+function changeDeroulement(partieId, deroulement) {
     const db = getDatabase();
-    const reference = ref(db, 'Partie'+ partieId);
+    const reference = ref(db, 'Partie' + partieId);
 
     // set(reference,{
     //     joueurs : joueurs,
@@ -28,18 +49,132 @@ function changeDeroulement(partieId, deroulement){
     // });
     update(reference, {
         deroulement: deroulement,
-    }); 
+    });
 }
-changeDeroulement("1","att"); 
+changeDeroulement("1", "att");
 
 
 const db = getDatabase();
 const reference = ref(db, 'Partie1/deroulement');
-onValue(reference,(snapshot)=>{
+onValue(reference, (snapshot) => {
     const data = snapshot.val();
     console.log(data);
     // Game(data);
 })
+
+
+
+
+
+
+
+
+function Start() {
+    changeDeroulement("1", "cupidon");
+    console.log("C’est la nuit, tout le village s’endort, les joueurs ferment les yeux");
+    return (
+        <h1>C’est la nuit, tout le village s’endort, les joueurs ferment les yeux</h1>
+    );
+}
+
+function Cupidon() {
+    changeDeroulement("1", "voyante");
+    console.log("Cupidon se réveille !");
+    return (
+      <>
+        <h1>Cupidon se réveille !</h1>
+      </>
+    );
+  }
+
+  function Voyante() {
+    changeDeroulement("1", "loup");
+    console.log("la Voyante se réveille, et désigne un joueur dont elle veut sonder la véritable personnalité !");
+    return (
+      <>
+        <h1>la Voyante se réveille, et désigne un joueur dont elle veut sonder la véritable personnalité !</h1>
+      </>
+    );
+  }
+
+  function Loups() {
+    changeDeroulement("1", "sorciere");
+    console.log(
+      "Les Loups-Garous se réveillent, se reconnaissent et désignent une nouvelle victime !!!"
+    );
+    return (
+      <h1>
+        Les Loups-Garous se réveillent, se reconnaissent et désignent une
+        nouvelle victime !!!
+      </h1>
+    );
+  }
+
+  function Sorciere() {
+    changeDeroulement("1", "jour"); 
+    console.log(
+      "La Sorcière se réveille, je lui montre la victime des Loups-Garous. Va-t-elle user de sa potion de guérison, ou d’empoisonnement ?"
+    );
+    return (
+      <h1>
+        La Sorcière se réveille, je lui montre la victime des Loups-Garous. Va-t-elle user de sa potion de guérison, ou d’empoisonnement ?
+      </h1>
+    );
+  }
+
+  function Jour() {
+    changeDeroulement("1", "chasseur"); 
+    console.log("C’est le matin, le village se réveille, tout le monde se réveille et ouvre les yeux…");
+    return (
+      <h1>
+        C’est le matin, le village se réveille, tout le monde se réveille et ouvre les yeux…
+      </h1>
+    );
+  }
+
+  function Chasseur() {
+    changeDeroulement("1", "maire"); 
+    console.log("Le chasseur joue");
+    return (
+      <h1>
+        Le chasseur joue
+      </h1>
+    );
+  }
+
+  function Maire({ changeDeroulement }) {
+    changeDeroulement("1", "voteVillage"); 
+    console.log("vote du nouveau maire");
+    return (
+      <h1>
+        vote du nouveau maire
+      </h1>
+    );
+  }
+  
+  function VoteVillage() {
+    changeDeroulement("1", "nuit");
+    console.log("vote de la personne à tuer");
+    return (
+      <h1>
+        vote de la personne à tuer
+      </h1>
+    );
+  }
+
+  function Nuit() {
+    console.log("C’est la nuit, tout le village s’endort, les joueurs ferment les yeux");
+    return (
+      <h1>
+        C’est la nuit, tout le village s’endort, les joueurs ferment les yeux
+      </h1>
+    );
+  }
+
+
+
+
+
 
 
 export default Game;
